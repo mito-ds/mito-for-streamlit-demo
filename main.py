ae7f8e2e90e1971fcb54389d79c2b89c70936a5e
Nate Rush <naterush1997@gmail.com>
2023-08-03 10:50:13 -0700

    make the page wide

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from mitosheet.streamlit.v1 import spreadsheet
 
+st.set_page_config(layout="wide")
+
 CSV_URL = 'https://raw.githubusercontent.com/plotly/datasets/master/tesla-stock-price.csv'
 new_dfs, code = spreadsheet(CSV_URL)
 
