97dfb58defb9682197a084bb1453a3ddb9112c28
Aaron Diamond-Reivich <aarondr77@gmail.com>
2024-02-14 12:26:05 -0500

    reduce memory use

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import streamlit as st
 from mitosheet.streamlit.v1 import spreadsheet
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
-CSV_URL = 'https://raw.githubusercontent.com/plotly/datasets/master/tesla-stock-price.csv'
-new_dfs, code = spreadsheet(CSV_URL)
+st.title('Try Mito: Use the spreadsheet below to generate Python')
 
-st.write(new_dfs)
+@st.cache_data
+def get_tesla_data():
+    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/tesla-stock-price.csv')
+    df = df.drop(0)
+    df['volume'] = df['volume'].astype(float)
+    return df
+
+tesla_data = get_tesla_data()
+
+new_dfs, code = spreadsheet(tesla_data)
 st.code(code)
