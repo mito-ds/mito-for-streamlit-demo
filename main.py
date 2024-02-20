287dc29edf731fbada291ee1007bf09dc7a391b3
Aaron Diamond-Reivich <aarondr77@gmail.com>
2024-02-20 16:28:41 +0100

    update comments

--- main.py
+++ main.py
@@ -23,11 +23,10 @@ st.code(code)
 def clear_mito_backend_cache():
     _get_mito_backend.clear()
 
-# Function to cache the last execution time - so we can clear
-# periodically
+# Function to cache the last execution time - so we can clear periodically
 @st.cache_resource
 def get_cached_time():
-    # Initialize with a dictionary to store the last execution hour
+    # Initialize with a dictionary to store the last execution time
     return {"last_executed_time": None}
 
 def try_clear_cache():
@@ -38,7 +37,7 @@ def try_clear_cache():
     current_time = datetime.now()
     cached_time = get_cached_time()
 
-    # Check if the current hour is different from the cached last execution hour
+    # Check if the current time is different from the cached last execution time
     if cached_time["last_executed_time"] is None or cached_time["last_executed_time"] + CLEAR_DELTA < current_time:
         clear_mito_backend_cache()
         cached_time["last_executed_time"] = current_time
